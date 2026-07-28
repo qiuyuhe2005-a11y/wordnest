1559944773a96900aede4866675a6688e478d91d
qiuyuhe2005-a11y <qiuyuhe2005@gmail.com>
2026-07-28 14:19:13 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wordnest-v1";
+const CACHE = "wordnest-v3";
 const APP = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP)));
